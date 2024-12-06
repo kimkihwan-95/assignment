b75e42b59b7578405c0655fd8afeae2988ff3823
kimkihwan-95 <ghkdka12@naver.com>
2024-12-06 16:11:39 +0900

    add folder

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/(repository 이름)',
 };
 
 export default nextConfig;

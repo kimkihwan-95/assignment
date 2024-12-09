60b8e7317e6024802118cb8d261e33ceafd22ede
kimkihwan-95 <ghkdka12@naver.com>
2024-12-09 16:07:10 +0900

    chore: nextconfig

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  // basePath: '/assignment',
+  output: 'export',
+  basePath: '/assignment',
 };
 
 export default nextConfig;
